1d3cdd0b9a35d290f6d266ae7983597c67122173
ragansu <ragansu@gmail.com>
2025-03-05 17:07:38 +0100

    update logo 2

--- docs/conf.py
+++ docs/conf.py
@@ -35,5 +35,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_logo = "_static/HiggsMLlogo.png.png"
-html_favicon = "_static/HiggsMLlogo.png.png"
+html_logo = "_static/HiggsMLlogo.png"
+html_favicon = "_static/HiggsMLlogo.png"
