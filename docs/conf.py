c04ddd61a2ea5370a9f014f3135f9d87c9981afa
ragansu <ragansu@gmail.com>
2025-05-16 14:23:11 +0200

    new tests

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ current_dir = os.path.abspath(os.path.dirname(__file__))
 image_dir = os.path.join(current_dir, "images")
 
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, image_dir)
+sys.path.insert(image_dir)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
